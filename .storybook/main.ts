07205e657d5fcafb056eb2ade5af9c8f6afb3c4f
Alasdair McLeay <alasdair.mcleay@me.com>
2023-03-23 16:42:28 +0000

    Downgrade to storybook 6

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,13 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
+  core: { builder: "@storybook/builder-vite" },
   docs: {
-    autodocs: 'tag',
+    autodocs: "tag",
   },
 };
-export default config;
+module.exports = config;
